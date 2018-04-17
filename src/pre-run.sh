fed7e69c75a88994044e5cd6b514b60e9adc3a17
Pawel Polanski <pawel@upday.com>
2018-04-17 14:22:20 +0200

    WIP

--- src/pre-run.sh
+++ src/pre-run.sh
@@ -8,7 +8,7 @@ adb shell "settings put global transition_animation_scale 0"
 adb shell "settings put global animator_duration_scale 0"
 adb shell "settings put global window_animation_scale 0"
 
-expect $HOME/src/save-snapshot.sh
+expect $HOME/src/save-snapshot.exp
 
 adb emu kill
 #kill -s SIGTERM $(cat $HOME/supervisord.pid)
